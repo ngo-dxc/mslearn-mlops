19ffbd57b7f51597b9e0ad19b3f34a9b8ecfe75e
ngo-dxc <134663157+ngo-dxc@users.noreply.github.com>
2023-07-20 10:44:56 +0000

    2nd commit

--- src/model/train.py
+++ src/model/train.py
@@ -34,13 +34,13 @@ def main(args):
 
         evaluate_model(model, X_test, y_test)
 
-    def get_csvs_df(path):
-        if not os.path.exists(path):
-            raise RuntimeError(f"Cannot use non-existent path provided: {path}")
-        csv_files = glob.glob(f"{path}/*.csv")
-        if not csv_files:
-            raise RuntimeError(f"No CSV files found in provided data path: {path}")
-        return pd.concat((pd.read_csv(f) for f in csv_files), sort=False)
+def get_csvs_df(path):
+    if not os.path.exists(path):
+        raise RuntimeError(f"Cannot use non-existent path provided: {path}")
+    csv_files = glob.glob(f"{path}/*.csv")
+    if not csv_files:
+        raise RuntimeError(f"No CSV files found in provided data path: {path}")
+    return pd.concat((pd.read_csv(f) for f in csv_files), sort=False)
 
 
 # TO DO: add function to split data
