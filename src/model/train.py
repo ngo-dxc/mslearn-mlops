61c79d259167ee4c647d9e857e697f97a85fc1b0
ngo-dxc <134663157+ngo-dxc@users.noreply.github.com>
2023-07-21 03:50:02 +0000

    code checks 9

--- src/model/train.py
+++ src/model/train.py
@@ -55,7 +55,7 @@ def split_data(df):
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
     # train model
-    model = LogisticRegression(C=1/reg_rate, 
+    model = LogisticRegression(C=1/reg_rate,
                                solver="liblinear").fit(X_train, y_train)
     return model
 
